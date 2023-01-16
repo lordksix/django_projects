cabd8ede38ce4fca71e56b1e4bbf0ea05953feaf
lordksix <121689749+lordksix@users.noreply.github.com>
2023-01-16 18:29:08 -0500

    Update settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nhzf&r^aw447ip97=4fodds!v83&6i!40=2%2yp@uq*)6!6l=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
